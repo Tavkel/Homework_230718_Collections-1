19d7a4a316a3d3cb2b30c2bf4bdd908fef53a0de
Tavkel <gmininb@gmail.com>
2023-07-19 15:44:51 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_230718_collections1/Homework230718Collections1Application.java
@@ -0,0 +1,13 @@
+package com.example.homework_230718_collections1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework230718Collections1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework230718Collections1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_230718_collections1/Homework230718Collections1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_230718_collections1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework230718Collections1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
